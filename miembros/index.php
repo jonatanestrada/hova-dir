a7f3d6349f1800234a18a5d7f76ddeeae93092a1
LuisEGR <luisegr149@gmail.com>
2017-03-09 12:50:55 -0600

    Ejemplo de menu

--- miembros/index.php
+++ miembros/index.php
@@ -1,5 +1,5 @@
 <?php
-include_once $_SERVER['DOCUMENT_ROOT']."/sistema/class/Menu.class.php"
+include_once $_SERVER['DOCUMENT_ROOT']."/sistema/class/Menu.class.php";
 
 Menu::start();
 echo "contenido aqui!!!";
