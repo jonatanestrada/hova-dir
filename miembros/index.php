75354cdf0fe883f13207c3d550d879871aeb56bf
LuisEGR <luisegr149@gmail.com>
2017-03-09 13:04:19 -0600

    menu insertado

--- miembros/index.php
+++ miembros/index.php
@@ -2,6 +2,6 @@
 include_once $_SERVER['DOCUMENT_ROOT']."/sistema/class/Menu.class.php";
 Menu::start(); //Inicia el menú
 /**** Aquí va todo el contenido, recomendable usar include_once ****/
-include_once $_SERVER['DOCUMENT_ROOT']."/dev/directorio/paginas/puestos.php";
+include_once $_SERVER['DOCUMENT_ROOT']."/dev/directorio/paginas/miembros.php";
 Menu::end(); // Esto despues del contenido
 ?>
