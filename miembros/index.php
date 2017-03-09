7a24e73c2d79787208312491854b9c91121f6530
LuisEGR <luisegr149@gmail.com>
2017-03-09 12:55:12 -0600

    reestructuración de carpetas

--- miembros/index.php
+++ miembros/index.php
@@ -1,8 +1,7 @@
 <?php
 include_once $_SERVER['DOCUMENT_ROOT']."/sistema/class/Menu.class.php";
-
-Menu::start();
-echo "contenido aqui!!!";
-Menu::end();
-
+Menu::start(); //Inicia el menú
+/**** Aquí va todo el contenido, recomendable usar include_once ****/
+include_once $_SERVER['DOCUMENT_ROOT']."/dev/directorio/paginas/puestos.php";
+Menu::end(); // Esto despues del contenido
 ?>
