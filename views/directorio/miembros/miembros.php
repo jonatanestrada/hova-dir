636ebf09823a8ce6741de53785d1c3058f860b94
jonatanestrada <jonatan.estrada@hovahealth.com>
2017-03-14 17:10:36 -0600

    Cambio prueba

--- views/directorio/miembros/miembros.php
+++ views/directorio/miembros/miembros.php
@@ -2,7 +2,7 @@
 
         <div class="col-sm-9 col-sm-offset-0 col-md-12 col-md-offset-0 main">
 
-			<h1 class="page-header">Empleados</h1>
+			<h1 class="page-header">Empleados_</h1>
 
 			
 			<button type="button" class="btn btn-default" onclick="location.href='agregar/'" data-dismiss="modal">Agregar empleado</button><br><br>
