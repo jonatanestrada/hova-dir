be8dcd094dd31cb4cc55c3f99baeb83cb61bd330
jonatanestrada <jonatan.estrada@hovahealth.com>
2017-03-22 11:07:03 -0600

    Cambio de titulo

--- views/directorio/miembros/modal_edit.php
+++ views/directorio/miembros/modal_edit.php
@@ -6,7 +6,7 @@
       <div class="modal-content">
         <div class="modal-header">
           <button type="button" class="close" data-dismiss="modal">&times;</button>
-          <h4 class="modal-title">Editar miembro</h4>
+          <h4 class="modal-title">Editar personal</h4>
         </div>
         <div class="modal-body">
 		          <div >
