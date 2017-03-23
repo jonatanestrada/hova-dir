a1bc4dd54222f5263e0d473ef255e6c0108c9410
jonatanestrada <jonatan.estrada@hovahealth.com>
2017-03-23 12:20:52 -0600

    Bug registro cambio user

--- classes/Miembro.class.php
+++ classes/Miembro.class.php
@@ -1,7 +1,7 @@
 <?php
 error_reporting(E_ALL & ~E_NOTICE);
 define(ROOT_DIR, $_SERVER['DOCUMENT_ROOT']);
-include_once "../../sistema/class/Usuarios.class.php";
+//include_once "../../sistema/class/Usuarios.class.php";
 include_once "base.api.php";
 include_once "Paginacion.class.php";
 
